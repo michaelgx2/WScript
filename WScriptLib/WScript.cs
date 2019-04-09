943f817989879463c591564590b0358fd744b1a8
michaelgx2 <michaelgx2@live.cn>
2019-04-09 10:58:02 +0800

    还剩个Loop

---
+++ WScriptLib/WScript.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WScriptLib
+{
+    public class WScript
+    {
+        public string ScrName = "";
+        public List<WScriptCmd> Cmds = new List<WScriptCmd>();
+        public Dictionary<string, WScript> Processes = new Dictionary<string, WScript>();
+        public static Dictionary<string, DelReplacement> StringReplacement = new Dictionary<string, DelReplacement>(); //全局设置，需要被替换的字符串转义符
+
+        public WScript() { }
+
+        public WScript(string script, string name = "")
+        {
+            ScrName = name;
+            ParseScript(script);
+        }
+
+        //解析分拆脚本文件
+        private void ParseScript(string script)
+        {
+            bool isComment = false;
+            bool isString = false;
+            bool isBlock = false;
+            int currentLine = 0;
+
+            string buff = "";
+            WScriptCmd temp = new WScriptCmd();
+            char last = char.MinValue;
+            int blockQuote = 0;//读取子代码块时里面包含多少大括号组
+            foreach (var c in script)
+            {
+                if (isString)
+                {
+                    //如果正在读取字符串
+                    if (c == '"')
+                    {
+                        //字符串结尾。
+                        if (isBlock)
+                        {
+                            //如果这是个子代码块
+                            buff += c;
+                        }
+                        else
+                        {
+                            //替换字符串
+                            foreach (var replace in StringReplacement)
+                            {
+                                buff = buff.Replace(replace.Key, replace.Value());
+                            }
+                            temp.Parameters.Add(buff); //加入字符串参数
+                            buff = "";
+                        }
+                        isString = false;
+                    }
+                    else
+                    {
+                        buff += c;
+                    }
+                }
+                else
+                {
+                    if (!isComment)
+                    {
+                        //如果正在读取代码而非注释
+                        if (c == '/')
+                        {
+                            if (last == '/')
+                            {
+                                isComment = true;
+                            }
+                            else
+                            {
+                                //应该不需要处理
+                            }
+                        }
+                        else if (c == '(')
+                        {
+                            if (isBlock)
+                            {
+                                //如果是子代码块，原样保留
+                                buff += c;
+                            }
+                            else
+                            {
+                                //函数名称结束，函数参数开始
+                                temp.CommandName = buff.Trim();
+                                buff = "";
+                            }
+                        }
+                        else if (c == ',')
+                        {
+                            if (isBlock)
+                            {
+                                //如果是子代码块 原样保留
+                                buff += c;
+                            }
+                            else
+                            {
+                                //一个参数的结尾
+                                float tempf = 0;
+                                long templ = 0;
+                                int tempi = 0;
+                                bool tempb = false;
+                                if (int.TryParse(buff, out tempi))
+                                {
+                                    temp.Parameters.Add(tempi);//Add int if can be converted to int
+                                }
+                                else if (long.TryParse(buff, out templ))
+                                {
+                                    temp.Parameters.Add(templ);//Add long if can be converted to long
+                                }
+                                else if (float.TryParse(buff, out tempf))
+                                {
+                                    temp.Parameters.Add(tempf);//Add float if can be converted to float
+                                }
+                                else if (bool.TryParse(buff, out tempb))
+                                {
+                                    temp.Parameters.Add(tempb);
+                                }
+                                else
+                                {
+                                    if (string.IsNullOrEmpty(buff.Trim()))
+                                    {
+                                        temp.Parameters.Add("&" + buff.Trim()); //Add as process name
+                                    }
+                                    else
+                                    {
+                                        //参数字符串为空，舍弃这个参数，因为如果这是个空字符串参数，上面已经处理完了
+                                    }
+                                }
+
+                                buff = "";
+                            }
+                        }
+                        else if (c == ')')
+                        {
+                            if (isBlock)
+                            {
+                                //如果是子代码块 原样保留
+                                buff += c;
+                            }
+                            else
+                            {
+                                //最后一个参数的结尾
+                                float tempf = 0;
+                                long templ = 0;
+                                int tempi = 0;
+                                bool tempb = false;
+                                if (int.TryParse(buff, out tempi))
+                                {
+                                    temp.Parameters.Add(tempi);//Add int if can be converted to int
+                                }
+                                else if (long.TryParse(buff, out templ))
+                                {
+                                    temp.Parameters.Add(templ);//Add long if can be converted to long
+                                }
+                                else if (float.TryParse(buff, out tempf))
+                                {
+                                    temp.Parameters.Add(tempf);//Add float if can be converted to float
+                                }
+                                else if (bool.TryParse(buff, out tempb))
+                                {
+                                    temp.Parameters.Add(tempb);
+                                }
+                                else
+                                {
+                                    if (string.IsNullOrEmpty(buff.Trim()))
+                                    {
+                                        temp.Parameters.Add("&" + buff.Trim()); //Add as process name
+                                    }
+                                    else
+                                    {
+                                        //参数字符串为空，舍弃这个参数，因为如果这是个空字符串参数，上面已经处理完了
+                                    }
+                                }
+
+                                buff = "";
+                            }
+                        }
+                        else if (c == ';')
+                        {
+                            if (isBlock)
+                            {
+                                //如果是子代码块 原样保留
+                                buff += c;
+                            }
+                            else
+                            {
+                                //一行代码的结尾
+                                Cmds.Add(temp);
+
+                                //重置全部标志位和临时变量
+                                isComment = false;
+                                isString = false;
+                                isBlock = false;
+                                temp = new WScriptCmd();
+                                buff = "";
+                                last = char.MinValue;
+                                blockQuote = 0;
+                            }
+                        }
+                        else if (c == '"')
+                        {
+                            //字符串开头
+                            isString = true;
+                            if (isBlock)
+                            {
+                                //如果是子代码块中的字符串，原样保留
+                                buff += c;
+                            }
+                            else
+                            {
+                                buff = "";
+                            }
+                        }
+                        else if (c == '{')
+                        {
+                            if (isBlock)
+                            {
+                                //如果是子代码块，原样保留并且计数
+                                buff += c;
+                                blockQuote++;
+                            }
+                            else
+                            {
+                                //开始一个子代码块
+                                isBlock = true;
+                                //临时记录子代码块函数名称
+                                temp.CommandName = buff.Trim();
+                                buff = "";
+                            }
+                        }
+                        else if (c == '}')
+                        {
+                            if (!isBlock)
+                            {
+                                throw new FormatException("Unexpected '}'");
+                            }
+                            else
+                            {
+                                if (blockQuote > 0)
+                                {
+                                    //依然在子代码块中，原样保留
+                                    buff += c;
+                                    blockQuote--;
+                                }
+                                else
+                                {
+                                    //子代码块的结尾，处理子代码块
+                                    if (Processes.ContainsKey(temp.CommandName))
+                                    {
+                                        throw new FormatException("The process name '" + temp.CommandName +
+                                                                  "' has existed in this script block. Line: " + currentLine);
+                                    }
+                                    else
+                                    {
+                                        Processes.Add("&" + temp.CommandName, new WScript(buff, temp.CommandName));
+                                    }
+
+                                    //重置全部标志位和临时变量
+                                    isComment = false;
+                                    isString = false;
+                                    isBlock = false;
+                                    temp = new WScriptCmd();
+                                    buff = "";
+                                    last = char.MinValue;
+                                    blockQuote = 0;
+                                }
+                            }
+                        }
+                        else if (c == '\r')
+                        {
+                            if (isBlock)
+                            {
+                                //如果是子代码块中的字符串，原样保留
+                                buff += c;
+                            }
+                        }
+                        else if (c == '\n')
+                        {
+                            if (isBlock)
+                            {
+                                //如果是子代码块中的字符串，原样保留
+                                buff += c;
+                            }
+                            currentLine++;
+                        }
+                        else
+                        {
+                            buff += c;
+                        }
+                    }
+                    else
+                    {
+                        //如果正在读取注释 则换行注释就结束
+                        if (c == '\n')
+                        {
+                            isComment = false;
+                            currentLine++;
+                        }
+                    }
+                }
+
+                last = c;
+            }
+        }
+    }
+
+    public class WScriptCmd
+    {
+        public string CommandName = "";
+        public List<object> Parameters = new List<object>();
+
+        public WScriptCmd() { }
+
+        public WScriptCmd(string cmd, params object[] paras)
+        {
+            CommandName = cmd;
+            foreach (var para in paras)
+            {
+                Parameters.Add(para);
+            }
+        }
+    }
+
+    public delegate string DelReplacement();
+}
---
+++ WScriptLib/WUScriptRunner.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WScriptLib
+{
+    public class WUScriptRunner
+    {
+        public List<ScrBranch> RuningScripts = new List<ScrBranch>();
+        public List<ScrBranch> RuningProcesses = new List<ScrBranch>();
+        public Dictionary<string, WSParam> Parameters = new Dictionary<string, WSParam>();
+
+        public ScrBranch CurrentScript { get { return RuningScripts.Count > 0 ? RuningScripts[0] : null; } }
+        public ScrBranch CurrentProcess { get { return RuningProcesses.Count > 0 ? RuningProcesses[0] : null; } }
+
+        private Dictionary<string, DelWScriptCallback> _callbacks = new Dictionary<string, DelWScriptCallback>();
+        private bool _paused = false;
+        private DelGetDeltaTime _deltaTime;
+
+        public WUScriptRunner(DelGetDeltaTime deltaTimeGetter)
+        {
+            PrepareReplacement();
+            PreparePresetFunction();
+            _deltaTime = deltaTimeGetter;
+        }
+
+        public void Update()
+        {
+            if (_paused) return;
+            if (CurrentProcess != null)
+            {
+                RunBranch(CurrentProcess);
+            }
+            else if (CurrentScript != null)
+            {
+                RunBranch(CurrentScript);
+            }
+        }
+
+        public void RunBranch(ScrBranch branch)
+        {
+            if (branch.NextLineTimer <= 0)
+            {
+                if (branch.CurrentLine >= branch.Script.Cmds.Count)
+                {
+                    (branch == CurrentScript ? RuningScripts : RuningProcesses).RemoveAt(0);
+                    Update();
+                    return;
+                }
+
+                WScriptCmd cmd = branch.Script.Cmds[branch.CurrentLine];
+                if (_callbacks.ContainsKey(cmd.CommandName))
+                {
+                    branch.NextLineTimer = _callbacks[cmd.CommandName](cmd);
+                    branch.CurrentLine++;
+                    if (branch.NextLineTimer <= 0)
+                    {
+                        //如果这个脚本不阻塞进程， 那立刻继续执行下一条
+                        Update();
+                    }
+                }
+                else
+                {
+                    throw new Exception("Didn't find function '" + cmd.CommandName + "' in system. Check spells of this script: " + (CurrentScript == null ? "" : CurrentScript.Script.ScrName));
+                }
+            }
+            else
+            {
+                branch.NextLineTimer -= _deltaTime?.Invoke() ?? 0;
+            }
+        }
+
+        public void RegisterCmd(string cmd, DelWScriptCallback callback)
+        {
+            _callbacks.Add(cmd, callback);
+        }
+
+        public void RegisterReplacement(string origin, DelReplacement replacer)
+        {
+            if (WScript.StringReplacement.ContainsKey(origin))
+            {
+                WScript.StringReplacement[origin] = replacer;
+            }
+            else
+            {
+                WScript.StringReplacement.Add(origin, replacer);
+            }
+        }
+
+        public void RunScript(WScript script, WUScrRunType runType = WUScrRunType.Append)
+        {
+            if (runType == WUScrRunType.Override)
+            {
+                RuningScripts.Clear();
+                RuningScripts.Add(new ScrBranch(script));
+            }
+            else if (runType == WUScrRunType.Insert)
+            {
+                RuningScripts.Insert(0, new ScrBranch(script));
+            }
+            else
+            {
+                RuningScripts.Add(new ScrBranch(script));
+            }
+        }
+
+        public void RunProcess(string proName)
+        {
+            if (CurrentScript != null && CurrentScript.Script.Processes.ContainsKey(proName))
+            {
+                RuningProcesses.Insert(0, new ScrBranch(CurrentScript.Script.Processes[proName]));
+            }
+            else
+            {
+                throw new Exception(proName + " does not exist in script '" + (CurrentScript == null ? "" : CurrentScript.Script.ScrName) + "'");
+            }
+        }
+
+        public void Pause()
+        {
+            _paused = true;
+        }
+
+        public void Resume()
+        {
+            _paused = false;
+        }
+
+        private void PreparePresetFunction()
+        {
+            RegisterCmd("runproc", (WScriptCmd line) =>
+            {
+                string processName = line.Parameters[0].ToString();
+                RunProcess(processName);
+                return 0;
+            });
+            RegisterCmd("wait", (WScriptCmd line) =>
+            {
+                float time = Convert.ToSingle(line.Parameters[0]);
+                return time;
+            });
+            //dim("key","变量类型(num/str/bool)"[,初始值]);//声明一个变量,初始值可以设为[$变量名]
+            RegisterCmd("dim", (WScriptCmd line) =>
+            {
+                string key = line.Parameters[0].ToString();
+                string type = line.Parameters[1].ToString();
+                object val = line.Parameters.Count > 2 ? GetParamVal(line.Parameters[2]) : null;
+                switch (type)
+                {
+                    case "number":
+                    case "num":
+                        Parameters.Add(key, new WSParam("num", val==null?Convert.ToSingle(val):0));
+                        RegisterReplacement("[$" + key + "]", () => Parameters[key].Value.ToString());
+                        RegisterReplacement("[$0" + key + "]", () => Convert.ToSingle(Parameters[key].Value).ToString("0"));
+                        RegisterReplacement("[$1" + key + "]", () => Convert.ToSingle(Parameters[key].Value).ToString("0.0"));
+                        RegisterReplacement("[$2" + key + "]", () => Convert.ToSingle(Parameters[key].Value).ToString("0.00"));
+                        break;
+                    case "string":
+                    case "str":
+                        Parameters.Add(key, new WSParam("str", val == null?"":val.ToString()));
+                        RegisterReplacement("[$" + key + "]", () => Parameters[key].Value.ToString());
+                        break;
+                    case "bool":
+                        Parameters.Add(key, new WSParam("bool", val != null && Convert.ToBoolean(val)));
+                        RegisterReplacement("[$" + key + "]", () => Convert.ToBoolean(Parameters[key].Value)?"YES":"NO");
+                        break;
+                }
+                return 0;
+            });
+            //set("key",修改方式(=|+|-|*|/|%),新值(字符串只支持=和+运算,bool值除了=以外的运算都做反转处理));//值可以设为[$变量名]
+            RegisterCmd("set", line =>
+            {
+                string key = line.Parameters[0].ToString();
+                string method = line.Parameters[1].ToString();
+                object val = GetParamVal(line.Parameters[2]);
+                if (Parameters.ContainsKey(key))
+                {
+                    var param = Parameters[key];
+                    switch (param.Type)
+                    {
+                        case "num":
+                            switch (method)
+                            {
+                                case "=":
+                                    param.Value = Convert.ToSingle(val);
+                                    break;
+                                case "+":
+                                    param.Value = Convert.ToSingle(param.Value) + Convert.ToSingle(val);
+                                    break;
+                                case "-":
+                                    param.Value = Convert.ToSingle(param.Value) - Convert.ToSingle(val);
+                                    break;
+                                case "*":
+                                    param.Value = Convert.ToSingle(param.Value) * Convert.ToSingle(val);
+                                    break;
+                                case "/":
+                                    param.Value = Convert.ToSingle(param.Value) / Convert.ToSingle(val);
+                                    break;
+                                case "%":
+                                    param.Value = Convert.ToSingle(param.Value) % Convert.ToSingle(val);
+                                    break;
+                            }
+                            break;
+                        case "str":
+                            switch (method)
+                            {
+                                case "=":
+                                    param.Value = val.ToString();
+                                    break;
+                                case "+":
+                                    param.Value = param.Value.ToString() + val.ToString();
+                                    break;
+                            }
+                            break;
+                        case "bool":
+                            switch (method)
+                            {
+                                case "=":
+                                    param.Value = Convert.ToBoolean(val);
+                                    break;
+                                default:
+                                    param.Value = !Convert.ToBoolean(val);
+                                    break;
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    throw new Exception("Parameter [" + key + "] does not exist.");
+                }
+                return 0;
+            });
+            //loop(循环子过程,循环次数[,"循环变量名"]);
+            RegisterCmd("loop", line =>
+            {
+
+                return 0;
+            });
+            //if("key",比较值(可以设为[$变量名]),"比较运算(=/>/</>=/<=/!=)",true子过程,false子过程);//字符串和布尔值只支持=
+            RegisterCmd("if", line =>
+            {
+                string key = line.Parameters[0].ToString();
+                object val = GetParamVal(line.Parameters[1]);
+                string method = line.Parameters[2].ToString();
+                string trueDo = line.Parameters[3].ToString();
+                string falseDo = line.Parameters[4].ToString();
+                var param = Parameters[key];
+                bool res = false;
+                switch (param.Type)
+                {
+                    case "num":
+                        switch (method)
+                        {
+                            case "=":
+                                res = Math.Abs(Convert.ToSingle(val) - Convert.ToSingle(param.Value)) < 0.000001f;
+                                break;
+                            case ">":
+                                res = Convert.ToSingle(param.Value) > Convert.ToSingle(val);
+                                break;
+                            case ">=":
+                                res = Convert.ToSingle(param.Value) >= Convert.ToSingle(val);
+                                break;
+                            case "<":
+                                res = Convert.ToSingle(param.Value) < Convert.ToSingle(val);
+                                break;
+                            case "<=":
+                                res = Convert.ToSingle(param.Value) <= Convert.ToSingle(val);
+                                break;
+                        }
+                        break;
+                    case "str":
+                        switch (method)
+                        {
+                            case "=":
+                                res = val.ToString() == param.Value.ToString();
+                                break;
+                        }
+                        break;
+                    case "bool":
+                        switch (method)
+                        {
+                            case "=":
+                                res = Convert.ToBoolean(val) == Convert.ToBoolean(param.Value);
+                                break;
+                        }
+                        break;
+                }
+                RunProcess(res?trueDo:falseDo);
+                return 0;
+            });
+        }
+
+        private object GetParamVal(object val)
+        {
+            string str = val.ToString();
+            if (str.StartsWith("[$") && str.EndsWith("]"))
+            {
+                string key = str.Substring(2, str.Length - 3);
+                return Parameters[key].Value;
+            }
+            else
+            {
+                return val;
+            }
+        }
+
+        private void PrepareReplacement()
+        {
+            RegisterReplacement("&n;", ()=>"\n");
+            RegisterReplacement("&q;", ()=>"\"");
+        }
+    }
+
+    public class ScrBranch
+    {
+        public WScript Script;
+        public int CurrentLine = 0;
+        public float NextLineTimer = 0;
+
+        public ScrBranch(WScript script)
+        {
+            Script = script;
+            CurrentLine = 0;
+        }
+    }
+
+    public delegate float DelWScriptCallback(WScriptCmd line);
+    public delegate float DelGetDeltaTime();
+
+    public enum WUScrRunType
+    {
+        /// <summary>
+        /// Run at the end of the script flow
+        /// </summary>
+        Append,
+        /// <summary>
+        /// Clear all running/waiting scripts and run this one only
+        /// </summary>
+        Override,
+        /// <summary>
+        /// Pause running script and run this one first.
+        /// </summary>
+        Insert
+    }
+
+    public struct WSParam
+    {
+        public string Type;
+        public object Value;
+
+        public WSParam(string type, object value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+}
